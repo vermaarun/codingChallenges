221680031b31bd52c43b9d14cec6401130e62acb
vermaarun <34645748+vermaarun@users.noreply.github.com>
2019-08-09 17:04:48 +0900

    add check_unique_char_string
    Program to check if all the char in string are unique or not

---
+++ check_unique_char_string.py
@@ -0,0 +1,15 @@
+def is_string_unique(s):
+    if len(s) > 128:
+        return False
+    string_map = {}
+    for c in s:
+        if c in string_map:
+            return False
+        else:
+            string_map[c] = True
+    # print string_map
+    return True
+
+
+if __name__ == "__main__":
+    print is_string_unique("abcdef")
